98684ffc6927894797d5f6865217fb70c2c848c7
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-11-21 19:49:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://amitthakur045:Balindra123@cluster0.kztbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})

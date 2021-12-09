24eab77d60184bd7cfb1568bd69f9d31bd29944d
Amit Thakur <at7129652@gmail.com>
2021-12-09 22:09:16 +0530

    setup the JWT login in frontend

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,7 +3,9 @@ import * as api from "../api/index.js";
 
 export const signin = (formData, history) => async (dispatch) => {
     try {
-        //log in the user
+        const { data } = await api.signin(formData);
+    
+        dispatch({ type: AUTH, data });
         history("/"); 
     } catch (error) {
         console.log(error);
@@ -12,7 +14,9 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
     try {
-        //sign up the user
+        const { data } = await api.signup(formData);
+
+        dispatch({ type: AUTH, data});
         history("/"); 
     } catch (error) {
         console.log(error);
